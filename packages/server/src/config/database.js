91eebfad9891fdeba589395fb418b82ae12d91bd
Fábio Amorim <fabiosantosamorim2@gmail.com>
2023-04-16 12:53:43 -0300

    Configurações do banco de dados

--- packages/server/src/config/database.js
+++ packages/server/src/config/database.js
@@ -1,8 +1,10 @@
-export const dialect = 'postgres';
-export const host = '127.0.0.1';
-export const username = 'postgres';
-export const password = '12345';
-export const database = 'database_admcollege';
+import database from '../../config/database.json';
+
+export const dialect = database.DIALECT;
+export const host = database.HOST;
+export const username = database.USERNAME;
+export const password = database.PASSWORD;
+export const database = database.DATABASE;
 export const define = {
   timestamps: true,
   underscored: true
