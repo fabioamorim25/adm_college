d8d2dc7cc6573c7d5d3c69b321fd03c543c6cd29
Fábio Amorim <fabiosantosamorim2@gmail.com>
2023-05-07 18:39:03 -0300

    Funcionalidade: Registar um departamento

--- packages/server/src/app/controllers/DepartamentController/AuthDepartament.js
+++ packages/server/src/app/controllers/DepartamentController/AuthDepartament.js
@@ -1,16 +1,20 @@
-
+const {Departament} = require('../../models')
 
 
 const Register = async(req,res)=>{
 
-    
+    const {dep_name, dep_email, dep_password} = req.body;
 
     try {
-    
-
         
+        if(await Departament.findByPk(dep_email || dep_name)){
+            return res.status(400).json('O departamento já existe')
+        }
 
 
+        const departament = await Departament.create({dep_name, dep_email, dep_password})
+        res.send(departament)
+      
 
     } catch (error) {
         return res.status(400).send({error: 'Falha ao registar o departamento'})
