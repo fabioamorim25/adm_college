a3a8c39538eecc05c089ba2993cf9312de05fc9d
Fábio Amorim <fabiosantosamorim2@gmail.com>
2023-10-12 10:45:32 -0300

    Melhoria no tratamento de error da criação do professor

--- packages/web/src/app/api/registerProf/route.ts
+++ packages/web/src/app/api/registerProf/route.ts
@@ -16,19 +16,25 @@ export async function POST(request: NextRequest) {
   } = await request.json()
 
 
-  if (!prof_name || !email || !password || !prof_phone || !prof_status || !departamentId)
-    return NextResponse.json({ message: "Dados invalidos", type:"error"}, { status: 400 })
 
-  const submitProf = await fetch("http://localhost:5000/prof/register", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${session?.token}`
-    },
-    body: JSON.stringify({ prof_name, email, password, prof_phone, prof_status, departamentId })
-  })
+  try {
+    if (!prof_name || !email || !password || !prof_phone || !prof_status || !departamentId)
+      return NextResponse.json({ message: "Dados invalidos", type: "error" })
 
-  const response = await submitProf.json()
+    const submitProf = await fetch("http://localhost:5000/prof/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${session?.token}`
+      },
+      body: JSON.stringify({ prof_name, email, password, prof_phone, prof_status, departamentId })
+    })
 
-  return NextResponse.json(response)
+    const response = await submitProf.json()
+
+    return NextResponse.json(response)
+
+  } catch (error) {
+    return NextResponse.json({ message: "Error no servidor. Por favor tente mais tarde", type: "error" })
+  }
 }
