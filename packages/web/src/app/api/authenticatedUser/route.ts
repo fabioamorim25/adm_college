abd233677a083ce66cb62a458e01dee3aa4ee593
Fábio Amorim <fabiosantosamorim2@gmail.com>
2023-10-12 10:47:45 -0300

    Melhoria no tratamento de error da autenticação do user

---
+++ packages/web/src/app/api/authenticatedUser/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function POST(request: NextRequest) {
+  //pegar os dados da requisição
+  const data = await request.json()
+  // desestruturar 
+  const { dep_name, email, password } = data
+  //validar se esta faltando algum dado do formulario
+  if (!dep_name || !email || !password) {
+    return NextResponse.json({ msg: "Dados invalidos" })
+  }
+  try {
+    //ENVIAR DADOS PARA O BACK END
+    const submitUser = await fetch("http://localhost:5000/department/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ dep_name, email, password })
+    })
+    //receber apenas a resposta do back end
+    const user = await submitUser.json()
+
+    // retornar os dados do user
+    return NextResponse.json(user)
+
+
+  } catch (error) {
+    return NextResponse.json({ msg: "Error no servidor. Por favor tente mais tarde" })
+  }
+}
