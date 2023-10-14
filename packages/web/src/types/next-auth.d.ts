a3679fa5350a010e58da5048159012701e7000f7
Fábio Amorim <fabiosantosamorim2@gmail.com>
2023-10-14 11:46:39 -0300

    Atualização da tipagem da sessão do user

--- packages/web/src/types/next-auth.d.ts
+++ packages/web/src/types/next-auth.d.ts
@@ -3,10 +3,12 @@ import NextAuth from "next-auth/next";
 declare module 'next-auth' {
   interface Session {
     user: {
+      dep_name: ReactNode;
       id: string
       email: string
       name: string
     }
+    token:string
   }
 
 
