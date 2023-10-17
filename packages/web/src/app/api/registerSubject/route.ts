a9bee8591c61b75fd660730ff19885d5b4e7a3ef
Fábio Amorim <fabiosantosamorim2@gmail.com>
2023-10-17 09:44:19 -0300

    Rota front para back: Para criação da máteria

---
+++ packages/web/src/app/api/registerSubject/route.ts
@@ -0,0 +1,53 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth"
+import { nextAuthOptions } from "../auth/[...nextauth]/options";
+
+
+export async function POST(request: NextRequest) {
+
+  const session = await getServerSession(nextAuthOptions)
+
+  const { sub_name, sub_shift, sub_start_time, sub_stop_time, sub_description, sub_mandatory, sub_day } = await request.json()
+  const departamentId = session?.user.id
+
+  if (!sub_name || !sub_shift || !sub_start_time || !sub_stop_time || !sub_description || !sub_mandatory || !departamentId)
+    return NextResponse.json({ message: "Dados invalidos", type: "error" })
+
+  if (new Date(sub_start_time) >= new Date(sub_stop_time)) {
+    return NextResponse.json({ message: "A data ou a hora não é valida", type: "error" });
+  }
+
+
+
+  try {
+    const submitCourse = await fetch("http://localhost:5000/subject/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${session?.token}`
+      },
+      body: JSON.stringify({
+        sub_name,
+        sub_shift,
+        sub_start_time,
+        sub_stop_time,
+        sub_description,
+        sub_mandatory,
+        sub_day,
+        departamentId
+      })
+    })
+    
+    const response = await submitCourse.json();
+    
+    if (!submitCourse.ok) {      
+      return NextResponse.json({ message: response.message, type: response.type });
+    } else {
+      return NextResponse.json({ message: response.message, type: response.type })
+    }
+    
+  } catch (error) {
+    return NextResponse.json({ message: "Error no servidor. Por favor tente mais tarde", type: "error" })
+  }
+
+}
