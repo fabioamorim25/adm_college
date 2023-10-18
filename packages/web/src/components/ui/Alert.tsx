0f376a828045f6049b77624f568b43fe26d7d369
Fábio Amorim <fabiosantosamorim2@gmail.com>
2023-10-18 10:02:30 -0300

    atualização do componente alert

--- packages/web/src/components/ui/Alert.tsx
+++ packages/web/src/components/ui/Alert.tsx
@@ -9,7 +9,7 @@ function Alert({ message, type }: AlertProps) {
   const alertStyles = type === 'success' ? 'bg-green-100 text-green-600' : 'bg-red-100 text-red-800';
 
   return (
-    <div className={`p-4 rounded border ${alertStyles}`}>
+    <div className={`p-2 mb-2 text-center rounded border ${alertStyles}`}>
       {message}
     </div>
   );
