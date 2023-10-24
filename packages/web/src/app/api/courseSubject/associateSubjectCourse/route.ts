fe6a290c7466ce6cf7b9130e109d8d1e7b92b87d
Fábio Amorim <fabiosantosamorim2@gmail.com>
2023-10-24 09:02:53 -0300

    Rota para associar máteria a um curso

---
+++ packages/web/src/app/api/courseSubject/associateSubjectCourse/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth"
+import { nextAuthOptions } from "../../auth/[...nextauth]/options";
+
+
+export async function POST(request: NextRequest) {
+
+  const session = await getServerSession(nextAuthOptions)
+  let data = await request.json();
+
+  const { subjectName, courseName } = data
+
+  try {
+
+    if (!courseName) {
+      return NextResponse.json({ message: "Dados invalidos", type: "error" })
+    }
+
+    const associate = await fetch("http://localhost:5000/associate_Subject_Course", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${session?.token}`
+      },
+      body: JSON.stringify({ subjectName, courseName })
+    })
+
+    const response = await associate.json()
+
+    return NextResponse.json({ message: response.message, type: response.type })
+  } catch (error) {
+    return NextResponse.json({ message: "Error no servidor. Por favor tente mais tarde", type: "error" })
+  }
+}
