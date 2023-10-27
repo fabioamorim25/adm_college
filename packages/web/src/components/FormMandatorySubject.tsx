2ff5027a2c2164534d28ebdce4b9e44664fcd90a
Fábio Amorim <fabiosantosamorim2@gmail.com>
2023-10-27 10:56:06 -0300

    Formulario para preRequisito da máteria

---
+++ packages/web/src/components/FormMandatorySubject.tsx
@@ -0,0 +1,122 @@
+'use client'
+
+import React, { useEffect, useState } from "react"
+import { useWorkDataContext } from "@/context/contextAdmin/WorkDataStoreProvider"
+import Alert from "./ui/Alert"
+
+
+interface ISubject {
+  sub_name: string
+}
+
+interface IRequisite{
+  preRequisite: string
+  subjectName:any
+}
+interface Imessage {
+  message: string
+  type: string
+}
+
+export function FormMandatorySubject() {
+  //curso no contexto
+  const { courseName,subjectName } = useWorkDataContext();
+  
+  //lista de máterias
+  const [subject, setSubjects] = useState<ISubject[]>([])
+  // dados para enviar
+  const [data, setData] = useState<IRequisite>({ preRequisite: '', subjectName }) // [preRequisite materias vindas do back] [nameSubject esta no contexto]
+
+  const [msg, setMsg] = useState<Imessage>({ message: '', type: '' });
+
+
+  //1° REQUISIÇÃO PARA PEGAR TODOS AS MATERIAS DE UM CURSO (Passar o nome do curso)
+  async function getSubjects() {
+    if (!courseName)
+      return console.log("front front", courseName)    
+
+    const listSubject = await fetch('/api/courseSubject/listSubject', {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ courseName: courseName })
+    })
+    const sub = await listSubject.json()
+    return setSubjects(sub)
+  }
+  useEffect(() => {
+    if (courseName) {
+      getSubjects();
+    }
+  }, [courseName]);
+
+
+  //2° REQUSIÇÃO PARA ASSOCIAR A MATERIA DO CONTEXTO A UMA MÁTERIA (Passar o nome das duas máterias)
+  async function handleRegister(e: React.ChangeEvent<HTMLSelectElement>) {
+    e.preventDefault();
+    setData((prev) => {
+      return { ...prev, [e.target.name]: e.target.value, subjectName }
+    });
+  }
+  async function onRegisterRequisite(event: React.SyntheticEvent) {
+    event.preventDefault()
+    //REQUISIÇÃO PARA O BACKEND DO NEXT PARA REGISTRA O CURSO
+    const request = await fetch("/api/courseSubject/mandatorySubject", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    })
+    //pegar o resultado da requisição
+    const response = await request.json()
+    
+    if(response)
+      return setMsg({
+        message:response.message,
+        type:response.type
+    })
+  }
+
+
+  return (
+    <>
+
+      <form onSubmit={onRegisterRequisite} className="m-4 p-6 border rounded shadow">
+
+       {msg.message && <Alert message={msg.message} type={msg.type} />}
+
+        <div className="mb-4">
+          <select
+            name="preRequisite"
+            value={data.preRequisite}
+            onChange={handleRegister}
+            className="w-full px-3 py-2 mt-2 text-gray-900 bg-white-50 border rounded-md focus:border-purple-900 focus:ring-purple-600 
+             focus:outline-none focus:ring focus:ring-opacity-40"
+          >
+            {
+              Array.isArray(subject) && subject.length > 0 ? (
+                subject.map((subject, index) => (
+                  <option key={index}>{subject.sub_name}</option>
+                ))
+              ) : (
+                <option value=""> carregando a lista de máterias ... </option>
+              )
+            }
+
+          </select>
+        </div >
+
+        <button className="p-4 py-2 bg-gray-200 rounded-md hover:bg-purple-700">Registra Máteria</button>
+      </form>
+    </>
+  )
+}
+
+
+
+// {subjectName === null ? (
+//            <label htmlFor="courseName" className="block text-sm font-alt text-gray-800">A Máteria possui algum pre-requisito para ser cursada:</label>
+//         ) : (
+//           <label htmlFor="courseName" className="block  font-alt text-gray-800">
+//             A Máteria <span className=" text-purple-700 text-lg">{subjectName.subjectName}</span> possui algum pre-requisito para ser cursada:
+//          </label>
+//         )
+//        }
