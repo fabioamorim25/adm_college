21caedf593daf3c5c7be09ee0061b986a514177a
Fábio Amorim <amorimpg1996@gmail.com>
2024-01-14 12:30:52 -0300

    Atualização schema do prisma: Address

--- packages/server/src/app/utils/auth.token.js
+++ packages/server/src/app/utils/auth.token.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 
 export const generateToken = (userId,userRole) => {
   const token = jwt.sign({ id: userId,role: userRole}, process.env.SECRET, {
-    expiresIn: "3000000000000"
+    // expiresIn: "100000" //10 minutos
   })
   
   return token
