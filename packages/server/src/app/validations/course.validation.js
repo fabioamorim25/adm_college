c690f13db8fcdd115dd02d19da379f444fc1855a
Fábio Amorim <amorimpg1996@gmail.com>
2024-01-19 11:04:33 -0300

    Validations course: Tratamento de erro e disconectar do db

--- packages/server/src/app/validations/course.validation.js
+++ packages/server/src/app/validations/course.validation.js
@@ -1,12 +1,19 @@
 import { prisma } from "../../lib/prismaClient"
 
 // VALIDAR SE EXISTE OUTRO CURSO COM O MESMO NOME
-export const courseUnic = async(cou_name)=>{
-  const course = await prisma.course.findUnique({
-      where:{
-          cou_name
+export const courseUnic = async (cou_name) => {
+  try {
+    const course = await prisma.course.findUnique({
+      where: {
+        cou_name
       }
-  })
-  if(course)
-  return {message:'Já existe esse nome para um curso',type:'error'}
+    })
+    if (course)
+      return { message: 'Já existe esse nome para um curso', type: 'error' }
+
+  } catch (error) {
+    return { message: 'Erro na validação do nome do curso', type: 'error' }
+  } finally {
+    await prisma.$disconnect();
+  }
 }
