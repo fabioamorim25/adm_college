3fed971794310148c69163b6bc996515029ae4ee
Fábio Amorim <amorimpg1996@gmail.com>
2024-01-19 11:22:59 -0300

    Repository address: Tratamento de erro e disconectar do db

--- packages/server/src/app/repository/address.Repository.js
+++ packages/server/src/app/repository/address.Repository.js
@@ -2,63 +2,76 @@ import { prisma } from '../../lib/prismaClient'
 
 // CRIAR O DOCUMENTO E RETONAR O DADO
 export const createAddress = async (add_street, add_city, add_neighborhood, add_number, add_complement, studentName) => {
-
-    const address = await prisma.address.create({
-        data: {
-            add_street,
-            add_city,
-            add_neighborhood,
-            add_number,
-            add_complement,
-            student: {
-                connect: {
-                    stu_name: studentName
+    try {
+        await prisma.address.create({
+            data: {
+                add_street,
+                add_city,
+                add_neighborhood,
+                add_number,
+                add_complement,
+                student: {
+                    connect: {
+                        stu_name: studentName
+                    }
                 }
+            },
+            select: {
+                id: false,
+                add_street: false,
+                add_city: false,
+                add_neighborhood: false,
+                add_number: false,
+                add_complement: false,
+                studentName: false
             }
-        },
-        select: {
-            id: true,
-            add_street: true,
-            add_city: true,
-            add_neighborhood: true,
-            add_number: true,
-            add_complement: true,
-            studentName: true
-        }
-    });
+        });
 
-    return address;
+        return { message: 'O Curso foi criado com sucesso', type: 'success' }
+
+    } catch (error) {
+        return { message: 'Tivemos um erro ao criar o endereço', type: 'error' }
+    } finally {
+        await prisma.$disconnect();
+    }
 }
 
 //EDITAR DOCUMENTO DO ENDEREÇO
 export const editAddress = async (id, data) => {
-    const address = await prisma.address.findFirst({
-        where: {
-            studentId: id,
-        },
-    });
+    try {
 
-    if (address) {
-        const updatedAddress = await prisma.address.update({
+        const address = await prisma.address.findFirst({
             where: {
-                id: address.id,
-            },
-            data: data,
-            select: {
-                id: true,
-                add_street: true,
-                add_city: true,
-                add_neighborhood: true,
-                add_number: true,
-                add_complement: true,
-                studentId: true,
-                createdAt: true,
-                updatedAt: true,
+                studentId: id,
             },
         });
 
-        return updatedAddress;
-    } else {
-        return { msg: 'Registro não encontrado' }
+        if (address) {
+            await prisma.address.update({
+                where: {
+                    id: address.id,
+                },
+                data: data,
+                select: {
+                    id: false,
+                    add_street: false,
+                    add_city: false,
+                    add_neighborhood: false,
+                    add_number: false,
+                    add_complement: false,
+                    studentId: false,
+                    createdAt: false,
+                    updatedAt: false,
+                },
+            });
+
+            return { message: 'O Endereço foi editado com sucesso', type: 'success' }
+        } else
+            return { message: 'Registro não encontrado', type: 'success' }
+
+    } catch (error) {
+        return { message: 'Tivemos um erro ao editar o endereço', type: 'error' }
+    } finally {
+        await prisma.$disconnect();
     }
 }
