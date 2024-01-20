ad283fca4c8077573e9ddbf4ba77a956fc426788
Fábio Amorim <amorimpg1996@gmail.com>
2024-01-20 10:19:48 -0300

    Rota de listagem das informações dos cursos

---
+++ packages/web/src/app/api/getAll/listInfoCourses/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+
+import { nextAuthOptions } from "../../auth/[...nextauth]/options";
+
+
+
+export async function GET(request: NextRequest) {
+
+  const session = await getServerSession(nextAuthOptions)
+  const token = session?.token
+
+
+  try {
+
+    const response = await fetch("http://localhost:5000/infoCourses", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      },
+    })
+
+    if (response.ok)
+      var res = await response.json()
+
+    return NextResponse.json(res)
+
+  } catch (error) {
+    return NextResponse.json({ message: "Tivemos um error na listagem das informações dos cursos", type: "error" })
+  }
+}
