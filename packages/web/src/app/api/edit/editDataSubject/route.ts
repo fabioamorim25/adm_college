9b23e9b50140ca368d5adc937d065714e2b38773
Fábio Amorim <amorimpg1996@gmail.com>
2024-02-05 09:00:23 -0300

    Rota inicial: Edição da associação curso-matéria

--- packages/web/src/app/api/edit/editDataSubject/route.ts
+++ packages/web/src/app/api/edit/editDataSubject/route.ts
@@ -1,7 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getServerSession } from 'next-auth'
+import { IAssociationProps } from "admin";
 
+import { getServerSession } from 'next-auth';
 import { nextAuthOptions } from "../../auth/[...nextauth]/options";
+
+
+interface IDataProps {
+  sub_name: string;
+  sub_shift: string;
+  sub_start_time: string;
+  sub_stop_time: string;
+  sub_description: string;
+  sub_mandatory: string;
+
+  subjectName: string;
+  courseName: string[];
+  association: IAssociationProps[];
+}
 
 
 export async function PUT(request: NextRequest) {
@@ -13,18 +28,36 @@ export async function PUT(request: NextRequest) {
 
 
   if (!subjectId || !numberModel || !token || !departamentId) {
-    return NextResponse.json({ message: "MetaDatas invalidos2", type: "error" }, { status: 400 })
+    return NextResponse.json({ message: "MetaDatas invalidos", type: "error" }, { status: 400 })
   }
 
-  async function filterDataNecessary(numberModel: number, data: any) {
+  async function filterDataNecessary(numberModel: number, data: IDataProps) {
     if (numberModel == 1) {
       const { sub_name, sub_shift, sub_start_time, sub_stop_time, sub_description, sub_mandatory } = data;
       return { sub_name, sub_shift, sub_start_time, sub_stop_time, sub_description, sub_mandatory };
     }
-    // if (numberModel == 2) {
-    //   const {  } = data;
-    //   return {  };
-    // }
+    if (numberModel === 2) {
+      const { subjectName, courseName, association } = data;
+
+      const courseNameSet = new Set(courseName);
+
+      // Retorna um array com os dados que se repetem association e courseName
+      const duplicateItems = association.filter(course =>
+        courseNameSet.has(course.course.cou_name)
+      );
+
+      // Retorna um array com os dados que não se repetem [DOCUMENTO A CRIAR]
+      const nonDuplicateItems = courseName.filter(name =>
+        !association.some(course => course.course.cou_name === name)
+      ).map(name => ({ course: { cou_name: name } }));
+
+      // Retorna um array com os dados que não existem mais [DOCUMENTO A EXCLUIR]
+      const itemsNoLongerExist = association.filter(course =>
+        !courseNameSet.has(course.course.cou_name)
+      );
+
+      return { subjectName, itemAnalysis: { duplicateItems, nonDuplicateItems, itemsNoLongerExist } };
+    }
     // if (numberModel == 3) {
     //   const {  } = data;
     //   return {  };
@@ -49,9 +82,9 @@ export async function PUT(request: NextRequest) {
     })
 
     const response = await editData.json()
-   
-    return NextResponse.json({ message: response.message, type: response.type })
 
+    // return NextResponse.json({ message: response.message, type: response.type })
+    return NextResponse.json(response)
   } catch (error) {
     return NextResponse.json({ message: "Tivemos um error na edição da máteria", type: "error" })
   }
