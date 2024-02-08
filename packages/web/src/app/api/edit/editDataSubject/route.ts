38741623eb6906b0e45fc1ba8c5fb277cab1e020
Fábio Amorim <amorimpg1996@gmail.com>
2024-02-08 17:47:12 -0300

    Rota para editar a associação da matéria com curso

--- packages/web/src/app/api/edit/editDataSubject/route.ts
+++ packages/web/src/app/api/edit/editDataSubject/route.ts
@@ -24,7 +24,7 @@ export async function PUT(request: NextRequest) {
   const { subjectId, numberModel, data } = await request.json();
   const session = await getServerSession(nextAuthOptions);
   const token = session?.token;
-  const departamentId = session?.user.id
+  const departamentId = session?.user.id;
 
 
   if (!subjectId || !numberModel || !token || !departamentId) {
@@ -38,25 +38,16 @@ export async function PUT(request: NextRequest) {
     }
     if (numberModel === 2) {
       const { subjectName, courseName, association } = data;
-
       const courseNameSet = new Set(courseName);
-
-      // Retorna um array com os dados que se repetem association e courseName
-      const duplicateItems = association.filter(course =>
-        courseNameSet.has(course.course.cou_name)
-      );
-
       // Retorna um array com os dados que não se repetem [DOCUMENTO A CRIAR]
       const nonDuplicateItems = courseName.filter(name =>
         !association.some(course => course.course.cou_name === name)
       ).map(name => ({ course: { cou_name: name } }));
-
       // Retorna um array com os dados que não existem mais [DOCUMENTO A EXCLUIR]
       const itemsNoLongerExist = association.filter(course =>
         !courseNameSet.has(course.course.cou_name)
       );
-
-      return { subjectName, itemAnalysis: { duplicateItems, nonDuplicateItems, itemsNoLongerExist } };
+      return { subjectName, itemAnalysis: { nonDuplicateItems, itemsNoLongerExist } };
     }
     // if (numberModel == 3) {
     //   const {  } = data;
@@ -67,8 +58,9 @@ export async function PUT(request: NextRequest) {
   }
   const resultData = await filterDataNecessary(numberModel, data);
 
-  if (resultData.type === "error")
+  if (resultData.type === "error"){
     return NextResponse.json(resultData, { status: 400 })
+  }
 
 
   try {
@@ -83,8 +75,7 @@ export async function PUT(request: NextRequest) {
 
     const response = await editData.json()
 
-    // return NextResponse.json({ message: response.message, type: response.type })
-    return NextResponse.json(response)
+    return NextResponse.json({ message: response.message, type: response.type })
   } catch (error) {
     return NextResponse.json({ message: "Tivemos um error na edição da máteria", type: "error" })
   }
